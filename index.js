85aaad0c5ade73f0cba5becdf56b4ecf5a329726
kzhein <kyawzinhein6@gmail.com>
2022-06-19 13:49:24 +0630

    Show geolocation data

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
 app.enable('trust proxy');
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  const { ip } = req;
   let headers = '';
 
   Object.keys(req.headers).forEach(header => {
     headers += `${header}: ${req.headers[header]}<br/>`;
   });
 
+  let geolocation = '';
+
+  try {
+    const {
+      data: { status, lat, lon, country, city, isp, org },
+    } = await axios.get(`http://ip-api.com/json/${ip}`);
+
+    if (status === 'success') {
+      geolocation = `
+      Latitude: ${lat}<br/>
+      Longitude: ${lon}<br/>
+      Country: ${country}<br/>
+      City: ${city}<br/>
+      ISP: ${isp}<br/>
+      Organization: ${org}<br/>
+      `;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
   res.send(`
   <h2>IP</h2>
-  ${req.ip}<br/>
+  ${ip}<br/>
+  <h2>Geolocation</h2>
+  ${geolocation}
   <h2>Headers</h2>
   ${headers}
   `);
